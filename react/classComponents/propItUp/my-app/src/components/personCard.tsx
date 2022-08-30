98f73ead003ba8ba61f99c85122b5ef6fcf4c632
czmud <caleb.zmuda@protonmail.com>
2022-08-30 07:47:05 -0700

    upload age increment button component

--- react/classComponents/propItUp/my-app/src/components/personCard.tsx
+++ react/classComponents/propItUp/my-app/src/components/personCard.tsx
@@ -7,15 +7,30 @@ interface PersonCardProps{
     hairColor: string;
 }
 
-export class PersonCard extends Component<PersonCardProps>{
+interface PersonCardStates{
+    age: number;
+}
+
+export class PersonCard extends Component<PersonCardProps, PersonCardStates>{
+    constructor(props: PersonCardProps | Readonly<PersonCardProps>){
+        super(props);
+        this.state = {age: this.props.age};
+    }
     
-    
+    incrementAge = () => {
+        this.setState({age: this.state.age + 1});
+    }
+    fullName(): string{
+        return this.props.firstName + " " + this.props.lastName;
+    }
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ this.incrementAge }>Birthday Button for {this.fullName()} </button>
             </div>
         )
     }
